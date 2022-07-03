c3da88f0108d0974ee02eb57db28fcd2cbf04bc7
GorlovDanila <danildanil20030@gmail.com>
2022-07-03 12:57:12 +0300

    Correct.2

--- app/src/main/java/com/example/finish/MainActivity.kt
+++ app/src/main/java/com/example/finish/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.finish
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,27 +11,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button? = findViewById(R.id.btn_calculate)
+        val button: Button = findViewById(R.id.btn_calculate)
 
-        button?.setOnClickListener {
+        button.setOnClickListener {
             calculation()
         }
     }
 
-    @SuppressLint("SetTextI18n")
     private fun calculation() {
-        val name: EditText? = findViewById(R.id.et_person_name)
-        val weight: EditText? = findViewById(R.id.et_person_weight)
-        val height: EditText? = findViewById(R.id.et_person_height)
-        val age: EditText? = findViewById(R.id.et_person_age)
+        val name: EditText = findViewById(R.id.et_person_name)
+        val weight: EditText = findViewById(R.id.et_person_weight)
+        val height: EditText = findViewById(R.id.et_person_height)
+        val age: EditText = findViewById(R.id.et_person_age)
         val result: TextView = findViewById(R.id.tv_result)
 
         try {
-            if ((name == null) || (weight == null) || (height == null) || (age == null) || (name.text.length > 50) || (height.text.toString()
+            if ((name.text == null) || (weight.text == null) || (height.text == null) || (age.text == null) || (name.text.length > 50) || (height.text.toString()
                     .toInt() !in (1..249)) || (weight.text.toString()
                     .toDouble() !in (1.0..249.0)) || (age.text.toString().toInt() !in (1..149))
             ) {
-                result.text = "Данные введены некорректно"
+                result.text = getString(R.string.error)
             } else {
                 val res: Double =
                     88.36 + (13.4 * weight.text.toString()
@@ -41,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                 result.text = "Ответ: ${res.toInt()}"
             }
         } catch (e: Exception) {
-            result.text = "Данные введены некорректно"
+            result.text = getString(R.string.error)
         }
     }
 }
